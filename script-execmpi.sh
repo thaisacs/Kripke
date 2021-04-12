c2a49c18bba142332369e296622ad25abacb0e90
Thais Camacho <thaiscamachoo@gmail.com>
2021-04-12 12:05:02 -0300

    up

--- script-execmpi.sh
+++ script-execmpi.sh
@@ -2,13 +2,13 @@ DATASET=exp-results
 rm -rf $DATASET
 mkdir $DATASET
 
-sudo perf stat -o $DATASET/perf-asc-kripke.out ./build/bin/kripke.exe --niter 100
-/usr/bin/time -o $DATASET/time-asc-kripke.out -v ./build/bin/kripke.exe --niter 100
-ltrace -o $DATASET/mpi-asc-kripke.out ./build/bin/kripke.exe --niter 100
+app=./build/bin/kripke.exe --niter 100
 
-cd $DATASET
-cat mpi-asc-kripke.out | grep "MPI" > mpi2-asc-kripke.out
-rm mpi-asc-kripke.out
-mv mpi2-asc-kripke.out mpi-asc-kripke.out
+perf stat -o $DATASET/perf-asc-kripke.out $app
+/usr/bin/time -o $DATASET/time-asc-kripke.out -v $app
 
-#mpirun -n 2 ./lmp -var t 300 -echo screen -in lj/in.lj
+#ltrace -o $DATASET/mpi-asc-kripke.out ./build/bin/kripke.exe --niter 100
+#cd $DATASET
+#cat mpi-asc-kripke.out | grep "MPI" > mpi2-asc-kripke.out
+#rm mpi-asc-kripke.out
+#mv mpi2-asc-kripke.out mpi-asc-kripke.out
